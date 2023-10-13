63c0a3317cf831b567485a9c966790923c2f9a41
Fernando Gurgel <gurgelff@gmail.com>
2023-10-13 15:27:34 -0300

    [root/build] feat: adiciona os index.html do ex2 e 3 ao dist/

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 tsc
-cp src/ex1-quantidade-de-vogais/index.html dist/ex1-quantidade-de-vogais/index.html
+cp src/ex1-quantidade-de-vogais/index.html dist/ex1-quantidade-de-vogais/index.html #ex1
+cp src/ex2-manipulacao-array/index.html dist/ex2-manipulacao-array/index.html #ex2
+cp src/ex3-pagina-web/index.html dist/index.html #arquivo principal
