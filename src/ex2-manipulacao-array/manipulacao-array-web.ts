e9310a38e93fdf86289a29eda652366629fc4812
Fernando Gurgel <gurgelff@gmail.com>
2023-10-13 16:30:34 -0300

    [ex3/ex2/web] impr: use h3 como sub-titulo

--- src/ex2-manipulacao-array/manipulacao-array-web.ts
+++ src/ex2-manipulacao-array/manipulacao-array-web.ts
@@ -20,6 +20,10 @@ import {
 
 let root_element: HTMLDivElement | null = document.getElementById("root") as HTMLDivElement;
 
+let titulo_h3: HTMLHeadingElement = document.createElement("h3");
+titulo_h3.innerText = "Lista de Gênios";
+root_element.appendChild(titulo_h3);
+
 for (let genio of lista_genios) {
     let genio_element: HTMLDivElement = document.createElement("div");
     genio_element.classList.add("genio");
@@ -33,13 +37,15 @@ for (let genio of lista_genios) {
     let genio_bio_element: HTMLDivElement = document.createElement("div");
     genio_bio_element.classList.add("genio-bio");
 
-    genio_id_element.innerText = genio.id.toString();
-    genio_name_element.innerText = genio.name;
-    genio_bio_element.innerText = genio.bio;
+    genio_id_element.innerText = "ID: " + genio.id.toString();
+    genio_name_element.innerText = "Nome: " + genio.name;
+    genio_bio_element.innerText = "Biografia: " + genio.bio;
 
     genio_element.appendChild(genio_id_element);    
     genio_element.appendChild(genio_name_element);
     genio_element.appendChild(genio_bio_element);
 
     root_element.appendChild(genio_element);
+    let br_element: HTMLBRElement = document.createElement("br");
+    root_element.appendChild(br_element);
 }
