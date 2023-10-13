34cf94f55776d41209d63bd4e21296e5e9e66bf4
Fernando Gurgel <gurgelff@gmail.com>
2023-10-13 15:26:17 -0300

    [ex3/ex2/web] feat: arquivo js para integrar as funções do ex2

---
+++ src/ex2-manipulacao-array/manipulacao-array-web.ts
@@ -0,0 +1,45 @@
+import lista_genios from './manipulacao-array';
+
+import {
+    get_name_imperative,
+    get_bio_imperative,
+    remove_genio_imperative,
+    change_name_imperative,
+    change_bio_imperative,
+    change_id_imperative,
+} from '../ex2-manipulacao-array/functions/GenioImperativo';
+
+import {
+    get_bio_functional,
+    get_name_functional,
+    remove_genio_functional,
+    change_bio_functional,
+    change_id_functional,
+    change_name_functional,
+  } from '../ex2-manipulacao-array/functions/GenioFuncional';
+
+let root_element: HTMLDivElement | null = document.getElementById("root") as HTMLDivElement;
+
+for (let genio of lista_genios) {
+    let genio_element: HTMLDivElement = document.createElement("div");
+    genio_element.classList.add("genio");
+
+    let genio_id_element: HTMLDivElement = document.createElement("div");
+    genio_id_element.classList.add("genio-id");
+
+    let genio_name_element: HTMLDivElement = document.createElement("div");
+    genio_name_element.classList.add("genio-name");
+
+    let genio_bio_element: HTMLDivElement = document.createElement("div");
+    genio_bio_element.classList.add("genio-bio");
+
+    genio_id_element.innerText = genio.id.toString();
+    genio_name_element.innerText = genio.name;
+    genio_bio_element.innerText = genio.bio;
+
+    genio_element.appendChild(genio_id_element);    
+    genio_element.appendChild(genio_name_element);
+    genio_element.appendChild(genio_bio_element);
+
+    root_element.appendChild(genio_element);
+}
